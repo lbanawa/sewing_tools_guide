a0f2d17b8b6780af798eef1cf180d962706f1d8c
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-03-31 19:21:18 -0400

    design layout
    added tableview in ViewController, added a secondary viewcontroller named detailsVC and added imageview and labels

--- SewingToolsGuide/detailsVC.swift
+++ SewingToolsGuide/detailsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  detailsVC.swift
 //  SewingToolsGuide
 //
 //  Created by Lauren Banawa on 3/31/20.
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
+class detailsVC: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
