42b744d783258896b5bb907f0773b6533c2f801f
lbanawa <61660817+lbanawa@users.noreply.github.com>
2020-03-31 20:44:45 -0400

    tool model
    created Tool class with init method, added tool variables and toolArray

--- SewingToolsGuide/ViewController.swift
+++ SewingToolsGuide/ViewController.swift
@@ -17,6 +17,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        //Tool Objects
+        let tailorsHam = Tool(toolName: "Tailor's Ham", toolJob: "Iron curved surfaces", toolImage: UIImage(named: "tailorsham.jpg")!)
+        let rulers = Tool(toolName: "Rulers", toolJob: "Measure and draw lines on flat surfaces", toolImage: UIImage(named: "rulers.jpg")!)
+        let seamRipper = Tool(toolName: "Seam Ripper", toolJob: "Remove stitching", toolImage: UIImage(named: "seamripper.jpg")!)
+        let measuringTape = Tool(toolName: "Measuring Tape", toolJob: "Measure flat and 3D objects", toolImage: UIImage(named: "measuringtape.jpg")!)
+        let patternWeights = Tool(toolName: "Pattern Weights", toolJob: "Hold fabric and/or pattern paper flat", toolImage: UIImage(named: "patternweights.jpg")!)
+        
+        let toolArray = [tailorsHam, rulers, seamRipper, measuringTape, patternWeights]
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
